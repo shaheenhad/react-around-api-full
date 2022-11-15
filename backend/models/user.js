9d7e7056ca830a4986a70fcb457e63e11c40a8a0
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2022-11-15 15:12:58 -0500

    imp missing package

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const LoginError = require('../errors/LoginError');
 
 const userSchema = new mongoose.Schema({

fc84db6e4745f49febc5d9af6cf056fd809fb352
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2022-11-28 10:24:49 -0500

    update celebrate validate

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
@@ -43,7 +43,7 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
